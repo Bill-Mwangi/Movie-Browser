93ebd5f1ecc04644c8cb78cdfc2f3b2e035b24cf
Bill Mwangi <billmwangi156@gmail.com>
2022-05-25 12:00:58 +0300

    using navigation component

--- app/src/main/java/com/bill/moviebrowser/MainActivity.kt
+++ app/src/main/java/com/bill/moviebrowser/MainActivity.kt
@@ -20,30 +20,11 @@ class MainActivity : AppCompatActivity() {
     val toolbar: Toolbar = findViewById(R.id.toolbar)
     setSupportActionBar(toolbar)
 
-    mainFragment = MainFragment()
-    supportFragmentManager.beginTransaction().apply {
-      replace(R.id.main_frag_container, mainFragment)
-      commit()
-    }
-
     //Todo: Add the drawer layout buttons to the activity and implement onClickListener
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
     menuInflater.inflate(R.menu.menu_items, menu)
-
-//    val search = menu?.findItem(R.id.search_button)
-//    val searchView = search?.actionView as? SearchView
-//    searchView?.isSubmitButtonEnabled = true
-//    searchView?.setOnQueryTextListener(this)
     return true
   }
-
-//  override fun onQueryTextChange(newText: String?): Boolean {
-//    mainFragment.
-//  }
-//
-//  override fun onQueryTextSubmit(query: String?): Boolean {
-//    TODO("Not yet implemented")
-//  }
 }
