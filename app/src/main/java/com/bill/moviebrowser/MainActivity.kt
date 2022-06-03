f971a58518a1914dbbcbde10b8af9648b7edcd81
Bill Mwangi <billmwangi156@gmail.com>
2022-06-03 21:59:10 +0300

    bottom nav

--- app/src/main/java/com/bill/moviebrowser/MainActivity.kt
+++ app/src/main/java/com/bill/moviebrowser/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import com.bill.moviebrowser.fragments.MainFragment
 import com.example.moviebrowser.R
 import com.example.moviebrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
-  private lateinit var mainFragment: MainFragment
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -19,8 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     val toolbar: Toolbar = findViewById(R.id.toolbar)
     setSupportActionBar(toolbar)
-
-    //Todo: Add the drawer layout buttons to the activity and implement onClickListener
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
